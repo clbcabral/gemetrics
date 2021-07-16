bbf756bce8c181a8e7735f79e4a121ba6bd4284a
Cleber <alberto.cleber@gmail.com>
2021-07-16 16:08:05 -0300

    Esqueci dos serializers.

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,4 @@
-from core.models import Metric
+from core.models import Metric, Analysis
 from rest_framework import serializers
 
 
@@ -6,3 +6,9 @@ class MetricSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Metric
         fields = ['dataset', 'phenotype', 'accuracy', 'accuracy_sd', 'f1_score', 'f1_score_sd']
+
+
+class AnalysisSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Analysis
+        fields = ['dataset', 'cnn', 'accuracy', 'f1_score', 'size', 'num_layers', 'num_params', 'time']
