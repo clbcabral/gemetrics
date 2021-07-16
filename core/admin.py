ad32d5df0628e72b0e8234b26b035b6dc4ecfecc
Cleber <alberto.cleber@gmail.com>
2021-07-16 15:59:35 -0300

    Entidade de coleta das análises.

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from core.models import Metric
+from core.models import Metric, Analysis
 
 # Register your models here.
 
 class MetricAdmin(admin.ModelAdmin):
     list_filter = ('dataset',)
     list_display = ('dataset', 'phenotype', 'accuracy', 'accuracy_sd', 'f1_score', 'f1_score_sd')
+    search_fields = ('phenotype',)
   
 
+class AnalysisAdmin(admin.ModelAdmin):
+    list_filter = ('dataset', 'cnn')
+    list_display = ('dataset', 'cnn', 'accuracy', 'f1_score', 'size', 'num_layers', 'num_params', 'time')
+    search_fields = ('dataset', 'cnn')
+
+
 admin.site.register(Metric, MetricAdmin)
+admin.site.register(Analysis, AnalysisAdmin)
--- core/models.py
+++ core/models.py
@@ -17,3 +17,22 @@ class Metric(models.Model):
     class Meta:
         verbose_name_plural = 'Metrics'
         ordering = ['-dataset', '-accuracy', '-f1_score']
+
+
+class Analysis(models.Model):
+    
+    dataset = models.CharField(max_length=255, verbose_name='Dataset', null=False)
+    cnn = models.CharField(max_length=255, verbose_name='CNN', null=False)
+    accuracy = models.DecimalField(max_digits=22, decimal_places=20, verbose_name='Accuracy')
+    f1_score = models.DecimalField(max_digits=22, decimal_places=20, verbose_name='F1Score')
+    size = models.DecimalField(max_digits=22, decimal_places=2, verbose_name='Size')
+    num_layers = models.IntegerField(verbose_name='Num. layers')
+    num_params = models.IntegerField(verbose_name='Num. params')
+    time = models.DecimalField(max_digits=22, decimal_places=6, verbose_name='Time')
+
+    def __str__(self):
+        return self.dataset
+
+    class Meta:
+        verbose_name_plural = 'Analysis'
+        ordering = ['-dataset', '-cnn', '-accuracy', '-f1_score']
